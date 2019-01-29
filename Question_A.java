02b512ff789bc819cd3bb08687bf04376c5d8b25
ckdoan <carolynd1234@gmail.com>
2019-01-28 18:39:59 -0700

    Add files via upload

---
+++ Question_A.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+
+/**Ormuco Programming Test Question A
+ * The goal for this question is to write a program that accepts two lines (x1, x2) and (x3,x4) 
+ * on the x-axis and returns whether they overlap. As an example, (1,5) and (2,6) overlaps but not (1,5) and (6,8).
+ * @author: Carolyn Doan
+ */
+
+/** Test Cases:
+ * 0 0 0 0 // No overlap, wrong format
+ * 1 5 2 6 // Overlaps
+ * 1 5 5 7 // No overlap
+ * 2 6 1 5 // Overlaps
+ * 1 5 6 8 // No overlap
+ * -1 -5 -6 -7 // No overlap, , wrong format
+ * -4 -1 -8 -2 // Overlaps
+ * -4 -1 -2 2 // Overlaps
+ */
+public class Question_A {
+	  
+	/**For the purpose of this question, we will assume that it is 2 vertical lines on the same y coordinate or 0, 
+	   * and the line goes from L1_LowerBound to L1_UpperBound. 
+	   * We will also assume that if the end points touch, then that is not an overlap
+	   * @param args is the command line arguments 
+	   * @param args will contain the two lines, in the form of "Line1_LowerBound Line1_UpperBound Line2_LowerBound Line2_UpperBound" 
+	   * A sample input argument would be 1 2 3 4
+	   * The program will print out if the lines overlap or not
+	 */
+	
+	public static void main(String[] args)  {
+		if (args.length<4 || args.length>4) // Error checking
+		{
+			System.err.println("You have entered an invalid input");
+			System.exit(1);
+		}
+		
+		int L1_LowerBound, L1_UpperBound, L2_LowerBound, L2_UpperBound; 
+		try {
+			L1_LowerBound =  Integer.parseInt(args[0]); 
+			L1_UpperBound =  Integer.parseInt(args[1]); 
+			L2_LowerBound =  Integer.parseInt(args[2]); 
+			L2_UpperBound =  Integer.parseInt(args[3]); 
+			
+			//Checking for points and incorrect argument format
+			if (L1_LowerBound >= L1_UpperBound || L2_LowerBound >= L2_UpperBound) 
+			{
+				System.err.println("You have entered a point or incorrect line format");
+				System.exit(1);
+			}
+			
+			// Check if the two lines are the same
+			if (L1_LowerBound == L2_LowerBound && L1_UpperBound == L2_UpperBound)
+			{
+				System.err.println("Lines overlap"); 
+				return;
+			}
+			
+			// Checking if the lines are in range of each other
+			if ((L1_LowerBound < L2_LowerBound && L2_LowerBound < L1_UpperBound) 
+					|| (L1_LowerBound < L2_UpperBound && L2_UpperBound < L1_UpperBound))
+			{
+				System.err.println("Lines overlap"); 
+			}
+			else if ((L2_LowerBound < L1_LowerBound && L1_LowerBound < L2_UpperBound) 
+					|| (L2_LowerBound < L1_UpperBound && L1_UpperBound < L2_UpperBound)) 
+			{
+				System.err.println("Lines overlap"); 
+			}
+			else {
+				System.err.println("Lines do not overlap");
+			}
+				
+		}
+		catch(NumberFormatException e){
+			System.err.println("You have entered an invalid input");
+			System.exit(1);
+		}
+	
+	}
+		
+}
---
+++ Question_B.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+/**Ormuco Programming Test Question B
+ * The goal of this question is to write a software library that accepts 2 version string as input and returns whether one is greater than, 
+ * equal, or less than the other. As an example: “1.2” is greater than “1.1”. Please provide all test cases you could think of.
+ * @author: Carolyn Doan
+ */
+
+
+/** Test Cases:
+ * 1.2 1.1  -1
+ * 1.1 1.1.1 -1
+ * 3.0 2.9.9 1
+ * 1.1 1.1 0 
+ */
+
+public class Question_B {
+	
+	/**This method compares two version strings
+	   * @param vers1 is the version1 string
+	   * @param vers2 is the version2 string 
+	   * @return 0 if the two versions are equal
+	   * @return -1 if version1 is less than version2
+	   * @return 1 if version1 is greater than version2
+	 */
+	public static int versionCompare(String vers1 , String vers2) {
+		 String[] vers1Parts = vers1.split("\\.");
+	     String[] vers2Parts = vers2.split("\\.");
+	     
+	     int length = Math.max(vers1Parts.length, vers2Parts.length);
+	     for(int i = 0; i < length; i++) {
+	    	int temp1=0, temp2=0; 
+	    	
+	    	if ( i < vers1Parts.length) {
+	    		temp1 = Integer.parseInt(vers1Parts[i]);
+	    	}
+	    	else {
+	    		temp1 = 0;
+	    	}
+	    	
+	    	if ( i < vers2Parts.length) {
+	    		temp2 = Integer.parseInt(vers2Parts[i]);
+	    	}
+	    	else {
+	    		temp2 = 0; 
+	    	}
+	    	
+	        if(temp1 < temp2) {
+	            return -1;
+	        }
+	        if(temp1 > temp2) {
+	            return 1;
+	        }
+	     }
+	     return 0;
+	}
+	
+	/**
+	   * @param args is the command line arguments 
+	   * @param args will contain the two versions in the form of "version1 version2" 
+	   * A sample input argument would be 1.1 1.1.1
+	   * The program will print out the two versions and the comparison results. 
+	   		* 0 if the two versions are equal
+	   		*-1 if version1 is less than version2
+	   		* 1 if version1 is greater than version2	 
+	 */
+	public static void main(String[] args)  {
+		if (args.length<2 || args.length>2) // Error checking
+		{
+			System.err.println("You have entered an invalid input");
+			System.exit(1);
+		}
+		
+		String version1, version2; 
+		version1 = args[0];
+		version2 = args[1];
+		
+		if(!version1.matches("[0-9]+(\\.[0-9]+)*") || !version2.matches("[0-9]+(\\.[0-9]+)*") ) {
+			throw new IllegalArgumentException("Invalid version format");
+		}
+	         
+		System.out.println("Version 1: " + version1);
+		System.out.println("Version 2: " + version2);
+		
+		int result = versionCompare(version1, version2);
+		System.out.println("Comparison Result: " + result);
+		
+	}
+}
